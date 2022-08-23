badced345b74bcf26b0522d13a9263f0dab6ff2c
siya-pathak <siya040604@gmail.com>
2022-08-23 17:23:35 +0530

    Updates

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -6,7 +6,7 @@ const { cloudinary } = require("../cloudinary");
 
 
 module.exports.index = async (req, res) => {
-    const campgrounds = await Campground.find({});
+    const campgrounds = await Campground.find({}).populate('popupText');
     res.render('campgrounds/index', { campgrounds })
 }
 
@@ -24,6 +24,7 @@ module.exports.createCampground = async (req, res, next) => {
     campground.images = req.files.map(f => ({url: f.path, filename: f.filename}))
     campground.author = req.user._id;
     await campground.save();
+    console.log(campground);
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
@@ -54,12 +55,13 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
+    console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({url: f.path, filename: f.filename}));
     campground.images.push(...imgs);
     await campground.save();
     if(req.body.deleteImages) {
-        for (let filenam of req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
             await cloudinary.uploader.destroy(filename);
         }
        await campground.updateOne({$pull: {images: {filename: {$in: req.body.deleteImages}}}});
--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,11 +1,11 @@
 
     mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/light-v10', // style URL
-    center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 9, // starting zoom
-    projection: 'globe' // display the map as a 3D globe
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/light-v10', // style URL
+      center: campground.geometry.coordinates, // starting position [lng, lat]
+      zoom: 9, // starting zoom
+      projection: 'globe' // display the map as a 3D globe
     });
     map.on('style.load', () => {
     map.setFog({}); // Set the default atmosphere style
